21ca9b277e6405ad1d5b75bcedde30d0aff33455
sabbir772002 <sabbir772002@gmail.com>
2024-02-28 23:28:44 +0600

    S

--- web/views.py
+++ web/views.py
@@ -35,10 +35,42 @@ def home(request):
             # User authentication failed
             return HttpResponse("mile nai vai tomar username or password")  # Example response
     else:  
+           return render(request, 'home.html')
+
+    return render(request, 'home.html')@csrf_exempt
+def log_in(request):
+    if request.method == 'POST':
+        # Access form field values from request.POST
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # data = json.loads(request.body)
+        # username = data.get('username')
+        # password = data.get('password')
+
+        remember_me = request.POST.get('remember_me')  # Assuming you have a checkbox with the name 'remember_me'
+        # Do something with the form data (e.g., validate, authenticate user)
+        # Example: Authenticate user using Django's built-in authentication system
+        #print(username, password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            a_login(request, user)  # Assuming you have imported the login function
+            if request.user.is_authenticated:
+                print("User is authenticatedd at web")  # This line will be executed if the user is authenticated
+            else:
+                print("User is not authenticated at web")
+            return render(request, 'home.html')
+        else:
+            # User authentication failed
+            return HttpResponse("mile nai vai tomar username or password")  # Example response
+    else:  
            return render(request, 'login.html')
 
     return render(request, 'login.html')
-
+from django.contrib.auth import logout
+def logout_view(request):
+    logout(request)
+    return redirect('home')
 
 @csrf_exempt
 def friends(request):
