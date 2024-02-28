1e69b9608ba6e50c4731024f600e512a1a95080a
Irin222 <138356191+Irin222@users.noreply.github.com>
2024-02-28 15:12:10 +0600

    I

--- web/views.py
+++ web/views.py
@@ -90,7 +90,6 @@ def signup(request):
             return redirect('home')
         else:
             print("Failed to register:", response.text)
-            print("amra tomake khujssi")
                     
         return render(request, 'signup.html')
 
