5e021527c5951f56cf1463cafe8a318e4c3feda6
Irin222 <138356191+Irin222@users.noreply.github.com>
2024-02-28 16:39:46 +0600

    O

--- nostalgia/settings.py
+++ nostalgia/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "web",
     "api",
-    #'corsheaders',
+    'corsheaders',
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
